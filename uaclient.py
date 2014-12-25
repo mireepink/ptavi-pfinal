9cea0eecdeeae24f01153cceb71f267009866555
alvaromv83 <alvaromv83@gmail.com>
2014-12-25 20:33:30 +0100

    Añadida lectura de comandos en uaclient.py

---
+++ uaclient.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+"""
+Programa User Agent Client para comunicación SIP
+"""
+
+import socket
+import sys
+
+
+# Protocolo y versión
+PROTOCOL = "sip"
+VERSION = "SIP/2.0"
+
+# Parámetros del usuario.
+if len(sys.argv) != 4:
+    sys.exit("Usage: python uaclient.py config method option")
+else:
+    CONFIG = sys.argv[1]
+    METHOD = sys.argv[2].upper()
+    OPTION = sys.argv[3]
