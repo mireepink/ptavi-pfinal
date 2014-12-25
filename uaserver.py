f80672b7697fc027e80c2afe0cca8300acf2b759
alvaromv83 <alvaromv83@gmail.com>
2014-12-25 20:33:48 +0100

    Añadida lectura de comandos en uaserver.py

---
+++ uaserver.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+"""
+Clase (y programa principal) para un servidor de eco en UDP simple
+"""
+
+import SocketServer
+import sys
+import os
+
+
+class EchoHandler(SocketServer.DatagramRequestHandler):
+    """
+    Echo server class
+    """
+
+    def handle(self):
+        """
+        Método para recibir en el manejador y establecer comunicación SIP
+        """
+        # Escribe dirección y puerto del cliente (de tupla client_address)
+        client_ip = str(self.client_address[0])
+        client_port = str(self.client_address[1])
+        print "IP cliente: " + client_ip + "| Puerto cliente: " + client_port
+
+        while 1:
+            # Leyendo línea a línea lo que nos envía el cliente
+            line = self.rfile.read()
+
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
+            else:
+                # Evaluación de los parámetros que nos envía el cliente
+                print "Recibido:\n" + line
+
+if __name__ == "__main__":
+
+    # Versión del protocolo SIP
+    MY_VERSION = "SIP/2.0"
+
+    # Evaluación de parámetros de la línea de comandos
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python uaserver.py config")
+    else:
+        CONFIG = sys.argv[1]
