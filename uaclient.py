7bcc765fd6513be70ae8e9f71f5f4eb5b4c5c137
alvaromv83 <alvaromv83@gmail.com>
2014-12-30 12:35:03 +0100

    Añadida funcionalidad de log en uaclient.py

--- uaclient.py
+++ uaclient.py
@@ -8,13 +8,27 @@ from xml.sax import make_parser
 import datahandler
 import socket
 import sys
+import time
 
 
+#--------------------------------- Métodos ------------------------------------
+def log2file(event):
+    """
+    Método para imprimir mensajes de log en un fichero de texto
+    """
+    logFile = open(LOG_FILE, 'a')
+    logFile.write('...\n')
+
+    formatTime = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+    logFile.write(formatTime + ' ' + event + '\n')
+    logFile.close()
+
+#-----------------------------Programa principal-------------------------------
 # Protocolo y versión
 PROTOCOL = "sip"
 VERSION = "SIP/2.0"
 
-# Parámetros del usuario.
+# Parámetros del usuario
 if len(sys.argv) != 4:
     sys.exit("Usage: python uaclient.py config method option")
 else:
@@ -22,33 +36,23 @@ else:
     METHOD = sys.argv[2].upper()
     OPTION = sys.argv[3]
 
-    # Parseo del fichero XML    
-    parser = make_parser()
-    dataHandler = datahandler.DataHandler()
-    parser.setContentHandler(dataHandler)
-    parser.parse(open(CONFIG))
+# Parseo del fichero XML    
+parser = make_parser()
+dataHandler = datahandler.DataHandler()
+parser.setContentHandler(dataHandler)
+parser.parse(open(CONFIG))
 
-    # Lectura del archivo de configuración UA
-    attr_dicc = {}
-    attr_dicc = dataHandler.get_attrs()
-    for attr in attr_dicc.keys():
-        if attr == 'username':
-            USERNAME = attr_dicc[attr]
-        elif attr == 'userpass':
-            USERPASS = attr_dicc[attr]
-        elif attr == 'servIp':
-            UASERV_IP = attr_dicc[attr]
-        elif attr == 'servPort':
-            UASERV_PORT = attr_dicc[attr]
-        elif attr == 'rtpPort':
-            RTP_PORT = attr_dicc[attr]
-        elif attr == 'proxIp':
-            PROX_IP = attr_dicc[attr]
-        elif attr == 'proxPort':
-            PROX_PORT = attr_dicc[attr]
-        elif attr == 'logPath':
-            LOG_PATH = attr_dicc[attr]
-        elif attr == 'audioPath':
-            AUDIO_PATH = attr_dicc[attr]
+# Lectura del archivo de configuración UA
+attr_dicc = dataHandler.get_attrs()
+USER_NAME = attr_dicc['userName']
+USER_PASS = attr_dicc['userPass']
+UASERV_IP = attr_dicc['servIp']
+UASERV_PORT = attr_dicc['servPort']
+RTP_PORT = attr_dicc['rtpPort']
+PROX_IP = attr_dicc['proxIp']
+PROX_PORT = attr_dicc['proxPort']
+LOG_FILE = attr_dicc['logPath']
+AUDIO_FILE = attr_dicc['audioPath']
 
-    print USERNAME, USERPASS, UASERV_IP, UASERV_PORT, RTP_PORT, PROX_IP, PROX_PORT, LOG_PATH, AUDIO_PATH
+# Comenzando el programa...
+log2file('Starting...')
