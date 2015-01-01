a6fb59a6e85220fbb342f30c7b17492ab5dcb58d
alvaromv83 <alvaromv83@gmail.com>
2015-01-01 12:26:58 +0100

    Registro de usuario sin log - proxy_registrar.py

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -11,6 +11,9 @@ import sys
 import os
 import time
 
+# Variable global para almacenar los usuarios y sus valores
+users = {}
+
 
 #--------------------------------- Clase --------------------------------------
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -22,10 +25,9 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         """
         Método para recibir en el manejador y establecer comunicación SIP
         """
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        client_ip = str(self.client_address[0])
-        client_port = str(self.client_address[1])
-        print "IP cliente: " + client_ip + "| Puerto cliente: " + client_port
+        # IP y puerto del cliente (de tupla client_address)
+        clientIp = str(self.client_address[0])
+        clientPort = str(self.client_address[1])
 
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -36,7 +38,91 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             else:
                 # Evaluación de los parámetros que nos envía el cliente
-                print "Recibido:\n" + line
+                print "\nRecibido de " + clientIp + "|" + clientPort + ':\n' \
+                      + line
+                try:
+                    parameters = line.split()
+                    method = parameters[0]
+                    protocol = parameters[1].split(':')[0]
+                    address = parameters[1].split(':')[1]
+                    login = address.split('@')[0]
+                    server_ip = address.split('@')[1]
+                    client_version = parameters[2]
+                    option = parameters[4]
+                    if protocol != 'sip' or client_version != 'SIP/1.0'\
+                        and client_version != 'SIP/2.0':
+                        response = MY_VERSION + " 400 Bad Request\r\n\r\n"
+                        self.wfile.write(response)
+                        print "Enviado:\n" + response
+                        break
+                except:
+                    response = MY_VERSION + " 400 Bad Request\r\n\r\n"
+                    self.wfile.write(response)
+                    print "Enviado:\n" + response
+                    break
+
+                # Evaluación del método que nos envía el cliente
+                if method == 'REGISTER':
+                    expires = float(option)
+
+                    # Comprobamos caducidad de usuarios registrados
+                    self.check_expires()
+
+                    # Registro del usuario
+                    if expires != 0:
+                        users[address] = (clientIp, expires, time.time(),
+                                          clientPort)
+                        self.register2file()
+                        print "Añadido el usuario " + address
+                    # Borrado del usuario (si existe en el diccionario)
+                    else:
+                        found = 0
+                        for user in users:
+                            if address == user:
+                                found = 1
+                        if found:
+                            del users[address]
+                            self.register2file()
+                            print "Eliminado el usuario " + address
+                        else:
+                            print "El usuario no se encuentra en el registro"
+                    self.wfile.write(MY_VERSION + " 200 OK\r\n\r\n")
+
+    def register2file(self):
+        """
+        Método para imprimir los usuarios registrados en un fichero de texto
+        """
+        users_file = open('registered.txt', 'w')
+        users_file.write('User' + '\t\t\t\t' + 'IP' + '\t\t\t' + 'Port' + '\t' + \
+                         'Log Time' + '\t\t' + 'Expires' + '\n')
+        for user in users:
+            ip = str(users[user][0])
+            expires = str(users[user][1])
+            log_time = str(users[user][2])
+            port = str(users[user][3])
+            users_file.write(user + "\t" + ip + "\t" + port + "\t" + log_time \
+                             + "\t" + expires + "\n")
+        users_file.close()
+
+    def check_expires(self):
+        """
+        Método para comprobar caducidad de usuarios registrados
+        """
+        addresses = []
+        for user in users:
+            addresses.append(user)
+        for address in addresses:
+            expires = users[address][1]
+            log_time = users[address][2]
+            elapsed_time = time.time() - log_time
+
+            # Si ha expirado el tiempo eliminamos al usuario
+            if elapsed_time >= expires:
+                del users[address]
+                self.register2file()
+                print "Tiempo expirado para " + address,
+                print "--> Usuario eliminado."
+
 
 #--------------------------------- Métodos ------------------------------------
 def log2file(event):
@@ -71,11 +157,16 @@ if __name__ == "__main__":
     # Lectura del archivo de configuración UA
     attr_dicc = dataHandler.get_attrs()
     SERVERNAME = attr_dicc['servName']
-    PRSERVER_IP = attr_dicc['servIp']
-    PRSERV_PORT = attr_dicc['servPort']
+    MY_IP = attr_dicc['servIp']
+    MY_PORT = int(attr_dicc['servPort'])
     REG_FILE = attr_dicc['regPath']
     REGPASS_FILE = attr_dicc['regPasswdPath']
     LOG_FILE = attr_dicc['logPath']
 
     # Comenzando el programa...
     log2file('Starting...')
+
+    # Creamos servidor de eco y escuchamos
+    serv = SocketServer.UDPServer((MY_IP, MY_PORT), EchoHandler)
+    print "Server " + SERVERNAME + " listening at port " + str(MY_PORT) + "..."
+    serv.serve_forever()
