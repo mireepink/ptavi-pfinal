2325d456e187aa4522b956d21721f12b5963b6c5
alvaromv83 <alvaromv83@gmail.com>
2015-01-01 12:26:17 +0100

    Registro de usuario sin log - uaclient.py

--- uaclient.py
+++ uaclient.py
@@ -24,8 +24,6 @@ def log2file(event):
     logFile.close()
 
 #-----------------------------Programa principal-------------------------------
-# Protocolo y versión
-PROTOCOL = "sip"
 VERSION = "SIP/2.0"
 
 # Parámetros del usuario
@@ -44,15 +42,41 @@ parser.parse(open(CONFIG))
 
 # Lectura del archivo de configuración UA
 attr_dicc = dataHandler.get_attrs()
-USER_NAME = attr_dicc['userName']
-USER_PASS = attr_dicc['userPass']
+MY_USERNAME = attr_dicc['userName']
+MY_USERPASS = attr_dicc['userPass']
 UASERV_IP = attr_dicc['servIp']
-UASERV_PORT = attr_dicc['servPort']
-RTP_PORT = attr_dicc['rtpPort']
+UASERV_PORT = int(attr_dicc['servPort'])
+RTP_PORT = int(attr_dicc['rtpPort'])
 PROX_IP = attr_dicc['proxIp']
-PROX_PORT = attr_dicc['proxPort']
+PROX_PORT = int(attr_dicc['proxPort'])
 LOG_FILE = attr_dicc['logPath']
 AUDIO_FILE = attr_dicc['audioPath']
 
 # Comenzando el programa...
 log2file('Starting...')
+
+#-----------------------------------Registro-----------------------------------
+if METHOD == 'REGISTER':
+    servIP = PROX_IP
+    servPort = PROX_PORT
+
+    # Creamos el socket, lo configuramos y lo atamos al servidor/puerto registrar
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((servIP, servPort))
+
+    # Contenido a enviar
+    request = METHOD + ' ' + 'sip:' + MY_USERNAME + '@dominio.net ' + VERSION \
+            + '\r\n' + 'Expires: ' + OPTION + '\r\n\r\n'
+
+# Enviamos solicitud y recibimos respuesta
+formatTime = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+my_socket.send(request)
+print "Enviado a " + str(servIP) + '|' + str(servPort) + ':\n' + request
+try:
+    response = my_socket.recv(1024)
+except socket.error:
+    print (formatTime + " Error: No server listening at " + servIP + " port " \
+           + str(servPort))
+    raise SystemExit
+print 'Recibido:\n' + response
