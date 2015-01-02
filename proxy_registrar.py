dda85e4319791e1a5e3c2ba76cb1580493f8a763
alvaromv83 <alvaromv83@gmail.com>
2015-01-02 13:36:35 +0100

    Añadida clase DataHandler en proxy_registrar

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 from xml.sax import make_parser
-import datahandler
+from xml.sax.handler import ContentHandler
 import SocketServer
 import sys
 import os
@@ -16,7 +16,7 @@ users = {}
 LOG_FILE = ""
 
 
-#--------------------------------- Clase --------------------------------------
+#-------------------------------- Clases --------------------------------------
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
@@ -27,13 +27,8 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         Método para recibir en el manejador y establecer comunicación SIP
         """
         # IP y puerto del cliente (de tupla client_address)
-        clientIp = str(self.client_address[0])
-        clientPort = str(self.client_address[1])
-
-        # Mensajes para depuración y log
-        sendTo = "Send to " + str(clientIp) + ':' + str(clientPort) + ':'
-        recvFrom = "Received from " + str(clientIp) + ':' + str(clientPort) \
-                 + ':'
+        self.clientIP = str(self.client_address[0])
+        self.clientPort = str(self.client_address[1])
 
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -44,89 +39,100 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             else:
                 # Evaluación de los parámetros que nos envía el cliente
-                log_debug('receive', clientIp, clientPort, request)
+                log_debug('receive', self.clientIP, self.clientPort, request)
                 try:
                     parameters = request.split()
-                    method = parameters[0]
+                    self.method = parameters[0]
                     protocol = parameters[1].split(':')[0]
-                    address = parameters[1].split(':')[1]
-                    login = address.split('@')[0]
-                    server_ip = address.split('@')[1]
+                    self.address = parameters[1].split(':')[1]
+                    login = self.address.split('@')[0]
+                    server_ip = self.address.split('@')[1]
                     client_version = parameters[2]
-                    option = parameters[4]
+                    self.option = parameters[4]
 
                 # Envío de "Bad Request"
                     if protocol != 'sip' or client_version != 'SIP/1.0'\
                         and client_version != 'SIP/2.0':
                         response = MY_VERSION + " 400 Bad Request\r\n\r\n"
                         self.wfile.write(response)
-                        log_debug('send', clientIp, clientPort, response)
+                        log_debug('send', self.clientIP, self.clientPort, response)
                         break
                 except:
                     response = MY_VERSION + " 400 Bad Request\r\n\r\n"
                     self.wfile.write(response)
-                    log_debug('send', clientIp, clientPort, response)
+                    log_debug('send', self.clientIP, self.clientPort, response)
                     break
 
-                # ------------------------- REGISTER --------------------------
-                if method == 'REGISTER':
-                    expires = float(option)
+                # Evaluación del método SIP recibido
+                self.checkmethod()
 
-                    # Comprobamos caducidad de usuarios registrados
-                    self.check_expires()
+    def checkmethod(self):
+        """
+        Método para evaluar método SIP recibido
+        """
+        # ------------------------- REGISTER ----------------------------------
+        if self.method == 'REGISTER':
+            expires = float(self.option)
 
-                    # Registro del usuario
-                    if expires != 0:
-                        users[address] = (clientIp, expires, time.time(),
-                                          clientPort)
-                        self.register2file()
-                        print "Añadido el usuario " + address
+            # Comprobamos caducidad de usuarios registrados
+            self.check_expires()
 
-                    # Borrado del usuario (si existe en el diccionario)
-                    else:
-                        found = 0
-                        for user in users:
-                            if address == user:
-                                found = 1
-                        # Usuario encontrado
-                        if found:
-                            del users[address]
-                            self.register2file()
-                            print "Eliminado el usuario " + address
-                        # Ususario no encontrado
-                        else:
-                            response = MY_VERSION \
-                                     + " 404 User Not Found\r\n\r\n"
-                            self.wfile.write(response)
-                            log_debug('send', clientIp, clientPort, response)
-                            break
+            # Registro del usuario
+            if expires != 0:
+                users[self.address] = (self.clientIP, expires, time.time(),
+                                          self.clientPort)
+                self.register2file()
+                print "Añadido el usuario " + self.address
+                # Envío de "OK"
+                response = MY_VERSION + " 200 OK\r\n\r\n"
+                self.wfile.write(response)
+                log_debug('send', self.clientIP, self.clientPort, response)
 
+            # Borrado del usuario (si existe en el diccionario)
+            else:
+                found = 0
+                for user in users:
+                    if self.address == user:
+                        found = 1
+                # Usuario encontrado
+                if found:
+                    del users[self.address]
+                    self.register2file()
+                    print "Eliminado el usuario " + self.address
                     # Envío de "OK"
                     response = MY_VERSION + " 200 OK\r\n\r\n"
                     self.wfile.write(response)
-                    log_debug('send', clientIp, clientPort, response)
-
-                # ------------------------- INVITE ----------------------------
-                elif method == 'INVITE':
-                    response = MY_VERSION + " 100 Trying\r\n\r\n"\
-                             + MY_VERSION + " 180 Ringing\r\n\r\n"\
-                             + MY_VERSION + " 200 OK\r\n\r\n"
-                    self.wfile.write(response)
-                    log_debug('send', clientIp, clientPort, response)
-
-                # -------------------- Método no permitido --------------------
+                    log_debug('send', self.clientIP, self.clientPort, response)
+                # Ususario no encontrado
                 else:
-                    response = MY_VERSION + " 405 Method Not Allowed\r\n\r\n"
+                    # Envío de "Not Found"
+                    response = MY_VERSION + " 404 User Not Found\r\n\r\n"
                     self.wfile.write(response)
-                    log_debug('send', clientIp, clientPort, response)
+                    log_debug('send', self.clientIP, self.clientPort, response)
+
+        # ------------------------- INVITE ------------------------------------
+        elif self.method == 'INVITE':
+            # Envío de "Trying, Ringing, OK"
+            response = MY_VERSION + " 100 Trying\r\n\r\n"\
+                     + MY_VERSION + " 180 Ringing\r\n\r\n"\
+                     + MY_VERSION + " 200 OK\r\n\r\n"
+            self.wfile.write(response)
+            log_debug('send', self.clientIP, self.clientPort, response)
+
+        # -------------------- Método no permitido ----------------------------
+        else:
+            # Envío de "Method Not Allowed"
+            response = MY_VERSION + " 405 Method Not Allowed\r\n\r\n"
+            self.wfile.write(response)
+            log_debug('send', self.clientIP, self.clientPort, response)
 
     def register2file(self):
         """
         Método para imprimir los usuarios registrados en un fichero de texto
         """
         users_file = open('registered.txt', 'w')
-        users_file.write('User' + '\t\t\t\t' + 'IP' + '\t\t\t' + 'Port' + '\t' + \
-                         'Log Time' + '\t\t' + 'Expires' + '\n')
+        users_file.write('User' + '\t\t\t\t' + 'IP' + '\t\t\t' + 'Port' + '\t'\
+                         + 'Log Time' + '\t\t' + 'Expires' + '\n')
         for user in users:
             ip = str(users[user][0])
             expires = str(users[user][1])
@@ -154,6 +160,44 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 self.register2file()
                 print "Tiempo expirado para " + address,
                 print "--> Usuario eliminado."
+
+class DataHandler(ContentHandler):
+    """
+    DataHandler class
+    """
+    def __init__(self):
+        """
+        Constructor. Inicializa el diccionario de atributos
+        """
+        self.attr_dicc = {}
+
+    def startElement(self, name, attrs):
+        """
+        Método que añade atributos al diccionario
+        """
+        if name == 'log':
+            path = attrs.get('path', "")
+            self.attr_dicc['logPath'] = path
+
+        if name == 'server':
+            name = attrs.get('name', "")
+            ip = attrs.get('ip', "")
+            puerto = attrs.get('puerto', "")
+            self.attr_dicc['servName'] = name
+            self.attr_dicc['servIp'] = ip
+            self.attr_dicc['servPort'] = puerto
+
+        if name == 'database':
+            path = attrs.get('path', "")
+            passwdpath = attrs.get('passwdpath', "")
+            self.attr_dicc['regPath'] = path
+            self.attr_dicc['regPasswdPath'] = passwdpath
+
+    def get_attrs(self):
+        """
+        Método que devuelve lista con atributos
+        """
+        return self.attr_dicc
 
 #--------------------------------- Métodos ------------------------------------
 def log_debug(oper, ip, port, msg):
@@ -189,7 +233,7 @@ if __name__ == "__main__":
 
     # Parseo del fichero XML
     parser = make_parser()
-    dataHandler = datahandler.DataHandler()
+    dataHandler = DataHandler()
     parser.setContentHandler(dataHandler)
     parser.parse(open(CONFIG))
 
