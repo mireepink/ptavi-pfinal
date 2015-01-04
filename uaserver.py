823168ab7592296ba3297da642129ea506d8a48c
alvaromv83 <alvaromv83@gmail.com>
2015-01-04 13:51:52 +0100

    Recepción de ACK en uaserver y envío de RTP

--- uaserver.py
+++ uaserver.py
@@ -12,6 +12,12 @@ import os
 import time
 
 
+# Variables globales
+MY_ADDRESS = ''
+MY_SERVIP = ''
+RTP_PORT = ''
+uaorig_tuple = ()
+
 #--------------------------------- Clases -------------------------------------
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -26,24 +32,28 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         self.clientIP = str(self.client_address[0])
         self.clientPort = str(self.client_address[1])
 
+        # Inicializando variables SDP
+        self.orig_address = ''
+        self.media_type = ''
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            request = self.rfile.read()
+            self.request = self.rfile.read()
 
             # Si no hay más líneas salimos del bucle infinito
-            if not request:
+            if not self.request:
                 break
             else:
                 # Evaluación parámetros obligatorios enviados por cliente
-                log_debug('receive', self.clientIP, self.clientPort, request)
+                log_debug('receive', self.clientIP, self.clientPort, self.request)
                 try:
-                    self.parameters = request.split()
-                    self.method = self.parameters[0]
-                    protocol = self.parameters[1].split(':')[0]
-                    self.address = self.parameters[1].split(':')[1]
-                    login = self.address.split('@')[0]
-                    server_ip = self.address.split('@')[1]
-                    client_version = self.parameters[2]
+                    self.request_list = self.request.split()
+                    self.method = self.request_list[0]
+                    protocol = self.request_list[1].split(':')[0]
+                    self.address = self.request_list[1].split(':')[1]
+                    user = self.address.split('@')[0]
+                    domain = self.address.split('@')[1]
+                    client_version = self.request_list[2]
 
                 # Envío de "Bad Request"
                     if protocol != 'sip' or client_version != 'SIP/1.0'\
@@ -67,12 +77,47 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         """
         # ------------------------- INVITE ------------------------------------
         if self.method == 'INVITE':
+            # Evaluación de cabecera y cuerpo del mensaje
+            try:
+                head = self.request[0:71]
+                body = self.request[71:]
+                head_list = head.split()
+                # Evaluación de parámetros SDP
+                if head_list[4] == 'application/sdp':
+                    sdp_list = body.split()
+                    self.orig_address = sdp_list[1].split('=')[1]
+                    uaorig_IP = sdp_list[2]
+                    self.media_type = sdp_list[5].split('=')[1]
+                    uaorig_mediaport = sdp_list[6]
+                    global uaorig_tuple
+                    uaorig_tuple = (uaorig_IP, uaorig_mediaport)
+            # Envío de "Bad Request"
+            except:
+                response = MY_VERSION + " 400 Bad Request\r\n\r\n"
+                self.wfile.write(response)
+                log_debug('send', self.clientIP, self.clientPort, response)
+                pass
             # Envío de "Trying, Ringing, OK"
             response = MY_VERSION + " 100 Trying\r\n\r\n"\
                      + MY_VERSION + " 180 Ringing\r\n\r\n"\
-                     + MY_VERSION + " 200 OK\r\n\r\n"
+                     + MY_VERSION + " 200 OK\r\n"\
+                     + 'Content-Type: application/sdp\r\n\r\n' + 'v=0\r\n'\
+                     + 'o=' + MY_ADDRESS + ' ' + MY_IP + '\r\n'\
+                     + 's=sesion_sip\r\n' + 't=0\r\n' + 'm=audio '\
+                     + str(RTP_PORT) + ' RTP\r\n'
             self.wfile.write(response)
             log_debug('send', self.clientIP, self.clientPort, response)
+
+        # ---------------------------- ACK ------------------------------------
+        elif self.method == 'ACK':
+            # --------------------- Envío RTP ---------------------------------
+            uaorig_IP = uaorig_tuple[0]
+            uaorig_mediaport = uaorig_tuple[1]
+            toRun = "./mp32rtp -i " + uaorig_IP + " -p " + uaorig_mediaport + " < " + AUDIO_FILE
+            log_debug('send', uaorig_IP, uaorig_mediaport, AUDIO_FILE)
+            print "Sending RTP content to client..."
+            os.system(toRun)
+            print "Finalizado envío RTP"
 
         # -------------------- Método no permitido ----------------------------
         else:
@@ -171,8 +216,8 @@ if __name__ == "__main__":
 
     # Lectura del archivo de configuración UA
     attr_dicc = dataHandler.get_attrs()
-    USER_NAME = attr_dicc['userName']
-    USER_PASS = attr_dicc['userPass']
+    MY_USERNAME = attr_dicc['userName']
+    MY_USERPASS = attr_dicc['userPass']
     MY_IP = attr_dicc['servIp']
     MY_PORT = int(attr_dicc['servPort'])
     RTP_PORT = attr_dicc['rtpPort']
@@ -180,6 +225,9 @@ if __name__ == "__main__":
     PROX_PORT = attr_dicc['proxPort']
     LOG_FILE = attr_dicc['logPath']
     AUDIO_FILE = attr_dicc['audioPath']
+
+    # Dirección SIP
+    MY_ADDRESS = MY_USERNAME + '@dominio.net'
 
     # Comenzando el programa...
     log_debug('', '', '', 'Starting...')
