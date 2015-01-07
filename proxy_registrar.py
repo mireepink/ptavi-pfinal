3f6377e6325b3a7b9033398a69369400c5ee3ea4
alvaromv83 <alvaromv83@gmail.com>
2015-01-07 17:01:24 +0100

    Depurado proxy_registrar - Finalizada parte basica

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -18,9 +18,9 @@ users = {}
 LOG_FILE = ""
 
 #-------------------------------- Clases --------------------------------------
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPProxyRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    Clase SIPProxyRegisterHandler. Recibe, procesa y reenvía mensajes SIP
     """
 
     def handle(self):
@@ -34,33 +34,32 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             self.request = self.rfile.read()
-
             # Si no hay más líneas salimos del bucle infinito
             if not self.request:
                 break
             else:
-                # Evaluación parámetros genéricos enviados por cliente
-                log_debug('receive', self.clientIP, self.clientPort, self.request)
+                # Evaluación parámetros obligatorios enviados por cliente
+                log_debug('receive', self.clientIP, self.clientPort,
+                          self.request)
                 try:
                     self.request_list = self.request.split()
                     self.method = self.request_list[0]
                     protocol = self.request_list[1].split(':')[0]
                     self.address = self.request_list[1].split(':')[1]
                     client_version = self.request_list[2]
-
-                # Envío de "Bad Request"
-                    if protocol != 'sip' or client_version != 'SIP/1.0'\
-                        and client_version != 'SIP/2.0':
-                        response = MY_VERSION + " 400 Bad Request\r\n\r\n"
-                        self.wfile.write(response)
-                        log_debug('send', self.clientIP, self.clientPort, response)
-                        break
+                # Excepción. Envío de "Bad Request"
                 except:
                     response = MY_VERSION + " 400 Bad Request\r\n\r\n"
                     self.wfile.write(response)
                     log_debug('send', self.clientIP, self.clientPort, response)
                     break
-
+                # Petición incorrecta. Envío de "Bad Request"
+                if protocol != 'sip' or client_version != 'SIP/1.0'\
+                    and client_version != 'SIP/2.0':
+                    response = MY_VERSION + " 400 Bad Request\r\n\r\n"
+                    self.wfile.write(response)
+                    log_debug('send', self.clientIP, self.clientPort, response)
+                    break
                 # Evaluación del método SIP recibido
                 self.checkmethod()
 
@@ -71,12 +70,17 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         # -------------------------- REGISTRO ---------------------------------
         if self.method == 'REGISTER':
             # Evaluación de puerto y parámetro opcional
-            server_port = self.request_list[1].split(':')[2]
-            expires = float(self.request_list[4])                                       # Contemplar "Bad Request"
-
+            try:
+                server_port = self.request_list[1].split(':')[2]
+                expires = float(self.request_list[4])
+            except:
+                # Envío de "Bad Request"
+                response = MY_VERSION + " 400 Bad Request\r\n\r\n"
+                self.wfile.write(response)
+                log_debug('send', self.clientIP, self.clientPort, response)
+                pass
             # Comprobamos caducidad de usuarios registrados
             self.check_expires()
-
             # Registro del usuario
             if expires != 0:
                 users[self.address] = (self.clientIP, expires, time.time(),
@@ -87,29 +91,28 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 response = MY_VERSION + " 200 OK\r\n\r\n"
                 self.wfile.write(response)
                 log_debug('send', self.clientIP, self.clientPort, response)
-
             # Borrado del usuario (si existe en el diccionario)
             else:
                 found = 0
                 for user in users:
                     if self.address == user:
                         found = 1
-                # Usuario encontrado
+                # Usuario encontrado en registro
                 if found:
-                    print "Deleted user " + self.address + ':' + users[self.address][3]
+                    print "Deleted user " + self.address + ':' \
+                          + users[self.address][3]
                     del users[self.address]
                     self.register2file()
                     # Envío de "OK"
                     response = MY_VERSION + " 200 OK\r\n\r\n"
                     self.wfile.write(response)
                     log_debug('send', self.clientIP, self.clientPort, response)
-                # Ususario no encontrado
+                # Ususario no encontrado en registro
                 else:
                     # Envío de "Not Found"
                     response = MY_VERSION + " 404 User Not Found\r\n\r\n"
                     self.wfile.write(response)
                     log_debug('send', self.clientIP, self.clientPort, response)
-
         # ----------------------- REENVÍO DE INVITE y BYE ---------------------
         elif self.method == 'INVITE' or self.method == 'BYE':
             found = 0
@@ -134,7 +137,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 response = MY_VERSION + " 404 User Not Found\r\n\r\n"
                 self.wfile.write(response)
                 log_debug('send', self.clientIP, self.clientPort, response)
-
         # ------------------------- REENVÍO DE ACK ----------------------------
         elif self.method == 'ACK':
             # Buscamos IP y puerto del UA destino
@@ -146,7 +148,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             send(my_socket, self.request, ua_destIP, ua_destPort)
             my_socket.close()
-
         # -------------------- Método no permitido ----------------------------
         else:
             # Envío de "Method Not Allowed"
@@ -181,7 +182,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             expires = users[address][1]
             log_time = users[address][2]
             elapsed_time = time.time() - log_time
-
             # Si ha expirado el tiempo eliminamos al usuario
             if elapsed_time >= expires:
                 del users[address]
@@ -189,9 +189,9 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 print "Tiempo expirado para " + address,
                 print "--> Usuario eliminado."
 
-class DataHandler(ContentHandler):
+class XMLHandler(ContentHandler):
     """
-    DataHandler class
+    Clase XMLHandler. Extrae etiquetas y atributos de un XML
     """
     def __init__(self):
         """
@@ -206,7 +206,6 @@ class DataHandler(ContentHandler):
         if name == 'log':
             path = attrs.get('path', "")
             self.attr_dicc['logPath'] = path
-
         if name == 'server':
             name = attrs.get('name', "")
             ip = attrs.get('ip', "127.0.0.1")
@@ -214,7 +213,6 @@ class DataHandler(ContentHandler):
             self.attr_dicc['servName'] = name
             self.attr_dicc['servIp'] = ip
             self.attr_dicc['servPort'] = puerto
-
         if name == 'database':
             path = attrs.get('path', "")
             passwdpath = attrs.get('passwdpath', "")
@@ -275,35 +273,30 @@ def receive(my_socket, servIP, servPort):
 
 #-----------------------------Programa principal-------------------------------
 if __name__ == "__main__":
-
     # Versión del protocolo SIP
     MY_VERSION = "SIP/2.0"
-
     # Evaluación de parámetros de la línea de comandos
     if len(sys.argv) != 2:
         sys.exit("Usage: python proxy_registrar.py config")
     else:
         CONFIG = sys.argv[1]
-
     # Parseo del fichero XML
     parser = make_parser()
-    dataHandler = DataHandler()
-    parser.setContentHandler(dataHandler)
+    xmlHandler_obj = XMLHandler()
+    parser.setContentHandler(xmlHandler_obj)
     parser.parse(open(CONFIG))
-
     # Lectura del archivo de configuración UA
-    attr_dicc = dataHandler.get_attrs()
+    attr_dicc = xmlHandler_obj.get_attrs()
     SERVERNAME = attr_dicc['servName']
     MY_IP = attr_dicc['servIp']
     MY_PORT = int(attr_dicc['servPort'])
     REG_FILE = attr_dicc['regPath']
     REGPASS_FILE = attr_dicc['regPasswdPath']
     LOG_FILE = attr_dicc['logPath']
-
     # Comenzando el programa...
     log_debug('', '', '', 'Starting...')
-
-    # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer((MY_IP, MY_PORT), EchoHandler)
-    print "Server " + SERVERNAME + " listening at " + MY_IP + ':' + str(MY_PORT) + "..."
+    # Creamos servidor SIP y escuchamos
+    serv = SocketServer.UDPServer((MY_IP, MY_PORT), SIPProxyRegisterHandler)
+    print "Server " + SERVERNAME + " listening at " + MY_IP + ':' \
+          + str(MY_PORT) + "..."
     serv.serve_forever()
