b551fd233d90000deab7f3bbb734f3fa9667b93e
javier baos mora <jbaos@zeta34.aulas.gsyc.es>
2015-01-09 21:14:58 +0100

    finalizado uaclient

--- uaclient.py
+++ uaclient.py
@@ -11,6 +11,7 @@ import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class ExtraerDatos(ContentHandler):
 
     def __init__(self):
@@ -35,13 +36,15 @@ class ExtraerDatos(ContentHandler):
     def get_tags(self):
         return self.lista
 
+
 def log(hora, accion, evento):
     fichero = ListaDatos[4][1]['path']
     fich = open(fichero, 'a')
-    fich.write(str(hora))
+    fich.write(time.strftime('%Y%m%d%H%M%S', time.gmtime(float(hora))))
     fich.write(accion)
     fich.write(evento)
     fich.close()
+
 
 if __name__ == "__main__":
 
@@ -52,14 +55,14 @@ if __name__ == "__main__":
     OPTION = entrada[3]
 
     if len(entrada) != 4:
-        sys.exit("Usage: python uaclient.py config method option")    
+        sys.exit("Usage: python uaclient.py config method option")
 
     parser = make_parser()
     Datos = ExtraerDatos()
     parser.setContentHandler(Datos)
     parser.parse(open(CONFIG))
     ListaDatos = Datos.get_tags()
-        
+
     IP_PR = ListaDatos[3][1]['ip']
     PUERTO_PR = int(ListaDatos[3][1]['puerto'])
 
@@ -72,14 +75,14 @@ if __name__ == "__main__":
         if METODO == 'REGISTER':
             #REGISTER sip:leonard@bigbang.org:1234 SIP/2.0
             LINE = METODO + ' sip:' + ListaDatos[0][1]['username']
-            LINE += ':' + ListaDatos[1][1]['puerto'] + ' SIP/2.0' + '\r\n'    
+            LINE += ':' + ListaDatos[1][1]['puerto'] + ' SIP/2.0' + '\r\n'
             #Expires: 3600
             LINE += 'Expires: ' + OPTION + '\r\n'
             hora = time.time()
-            accion = ' Send to ' + str(IP_PR) + ':' + str(PUERTO_PR)
+            accion = ' Sent to ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = LINE.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
 
         elif METODO == 'INVITE':
             #INVITE sip:penny@girlnextdoor.com SIP/2.0
@@ -89,7 +92,10 @@ if __name__ == "__main__":
             #v=0
             LINE += 'v=0' + '\r\n'
             #o=leonard@bigbang.org 127.0.0.1
-            LINE += 'o=' + ListaDatos[0][1]['username'] + ' ' + ListaDatos[1][1]['ip'] + '\r\n'
+            ip = ListaDatos[1][1]['ip']
+            if ip == "" or ip == " ":
+                ip = '127.0.0.1'
+            LINE += 'o=' + ListaDatos[0][1]['username'] + ' ' + ip + '\r\n'
             #s=misesion
             LINE += 's=misesion' + '\r\n'
             #t=0
@@ -97,27 +103,27 @@ if __name__ == "__main__":
             #m=audio 34543 RTP
             LINE += 'm=audio ' + ListaDatos[2][1]['puerto'] + ' RTP'
             hora = time.time()
-            accion = ' Send to ' + str(IP_PR) + ':' + str(PUERTO_PR)
+            accion = ' Sent to ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = LINE.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
 
         elif METODO == 'BYE':
             #BYE penny@girlnextdoor.com
             LINE = METODO + ' sip:' + OPTION + ' SIP/2.0'
             hora = time.time()
-            accion = ' Send to ' + str(IP_PR) + ':' + str(PUERTO_PR)
+            accion = ' Sent to ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = LINE.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
 
         else:
             LINE = METODO + ' sip:' + OPTION + ' SIP/2.0'
             hora = time.time()
-            accion = ' Send to ' + str(IP_PR) + ':' + str(PUERTO_PR)
+            accion = ' Sent to ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = LINE.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
 
         print "Enviando: " + LINE
         my_socket.send(LINE + '\r\n')
@@ -131,40 +137,40 @@ if __name__ == "__main__":
             accion = ' Received from ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = data.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
         elif len(sentencia) == 14:
-            Receptor_IP = sentencia[7]
+            Receptor_IP = sentencia[8]
             Receptor_IP = Receptor_IP.split(' ')
             Receptor_IP = Receptor_IP[1]
-            Receptor_Puerto = sentencia[10]
+            Receptor_Puerto = sentencia[11]
             Receptor_Puerto = Receptor_Puerto.split(' ')
             Receptor_Puerto = Receptor_Puerto[1]
             hora = time.time()
             accion = ' Received from ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = data.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
             fichero_audio = ListaDatos[5][1]['path']
             LINE = 'ACK sip:' + OPTION
-            LINE += ' SIP/2.0' + '\r\n'
+            LINE += ' SIP/2.0\r\n'
             print 'Enviando: ' + LINE
             my_socket.send(LINE + '\r\n')
             hora = time.time()
-            accion = ' Send to ' + str(IP_PR) + ':' + str(PUERTO_PR)
+            accion = ' Sent to ' + str(IP_PR) + ':' + str(PUERTO_PR)
             evento = LINE.replace('\r\n', ' ')
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
-            aEjecutar = "./mp32rtp -i " + str(Receptor_IP) + " -p " + str(Receptor_Puerto)
+            log(hora, accion, evento)
+            aEjecutar = "./mp32rtp -i " + str(Receptor_IP) + " -p "
+            aEjecutar += str(Receptor_Puerto)
             aEjecutar += " < " + fichero_audio
             os.system('chmod 755 mp32rtp')
             os.system(aEjecutar)
-            #comando = 'cvlc rtp://@' + str(Receptor_IP) + ':' + str(Receptor_Puerto)
-            #os.system(comando)
             hora = time.time()
-            accion = ' Send to ' + str(Receptor_IP) + ':' + str(Receptor_Puerto)
+            accion = ' Sent to ' + str(Receptor_IP) + ':'
+            accion += str(Receptor_Puerto)
             evento = 'Envio RTP'
             evento = ': ' + evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
             data = my_socket.recv(1024)
             print 'Recibido -- ', data
         else:
@@ -172,7 +178,7 @@ if __name__ == "__main__":
             accion = ' Error: '
             evento = data.replace('\r\n', ' ')
             evento = evento + '\r\n'
-            log(hora,accion,evento)
+            log(hora, accion, evento)
 
         print "Terminando socket..."
         my_socket.close()
@@ -183,4 +189,4 @@ if __name__ == "__main__":
         hora = time.time()
         accion = ' Error: No server listening at '
         evento = str(IP_PR) + ' port ' + str(PUERTO_PR) + '\r\n'
-        log(hora,accion,evento)
+        log(hora, accion, evento)
