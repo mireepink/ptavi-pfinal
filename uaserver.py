67b4475902f90453bf8ea771ca2fda99efd6aece
mireepink <mireypink@hotmail.com>
2015-01-09 14:08:13 +0100

    Pep8 y avanzadas

--- uaserver.py
+++ uaserver.py
@@ -9,6 +9,9 @@ import sys
 import os
 import os.path
 import socket
+import threading
+import time
+
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -30,18 +33,26 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 print 'SIP/2.0 405 Method Not Allowed' + '\r\n\r\n'
             elif Metod == 'INVITE':
                 Destinatario = lines.split(' ')[2]
-                Puerto_RTP = lines.split(' ')[12]
+                Puerto_RTP = lines.split(' ')[6]
                 print 'Mandamos...'
                 rcv_invite = 'SIP/2.0 100 Trying' + '\r\n\r\n'
                 rcv_invite += 'SIP/2.0 180 Ringing' + '\r\n\r\n'
-                rcv_invite += 'SIP/2.0 200 OK' + '\r\n\r\n' + "Content-Type: application/sdp \r\n\r\n" + "v=0 \r\n" + "o=" + USUARIO + " " + IP + ' \r\n' + "s=vampireando" + ' \r\n' + "t=0" + ' \r\n' + "m=audio " + str(PUERTO_AUDIO) + ' RTP' + '\r\n\r\n'
+                rcv_invite += 'SIP/2.0 200 OK' + '\r\n\r\n'
+                rcv_invite += "Content-Type: application/sdp \r\n\r\n"
+                rcv_invite += "v=0 \r\n" + "o=" + USUARIO + " " + IP + ' \r\n'
+                rcv_invite += "s=vampireando" + ' \r\n' + "t=0" + ' \r\n'
+                rcv_invite += "m=audio " + str(PUERTO_AUDIO) + ' RTP\r\n\r\n'
                 print rcv_invite
                 self.wfile.write(rcv_invite)
             elif Metod == 'ACK':
-                aAejecutar = './mp32rtp -i ' + IP + ' -p ' + str(Puerto_RTP) + ' < ' + PATH_AUDIO
+                t = MiThread(IP, Puerto_RTP)
+                t.start()
+                t.join()
+                aAejecutar = './mp32rtp -i ' + IP + ' -p '
+                aAejecutar += str(Puerto_RTP) + ' < ' + PATH_AUDIO
                 print 'Vamos a ejecutar', aAejecutar
                 os.system(aAejecutar)
-                print 'Ejecutado', '\r\n\r\n' 
+                print 'Ejecutado', '\r\n\r\n'
             elif Metod == 'BYE':
                 self.wfile.write('SIP/2.0 200 OK' + '\r\n\r\n')
                 print 'Terminando conversación...'
@@ -49,8 +60,25 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 self.wfile.write('SIP/2.0 400 Bad Request' + '\r\n\r\n')
                 print 'SIP/2.0 400 Bad Request' + '\r\n\r\n'
 
+
+class MiThread(threading.Thread):
+    """
+    Thread class
+    """
+
+    def __init__(self, IP, Puerto_RTP):
+        threading.Thread.__init__(self)
+        self.IP = IP
+        self.Puerto_RTP = Puerto_RTP
+
+    def run(self):
+        aAejecutar1 = 'cvlc rtp://@' + self.IP + ':'
+        aAejecutar1 += str(self.Puerto_RTP) + ' &'
+        print 'Vamos a ejecutar', aAejecutar1
+        os.system(aAejecutar1)
+
 if __name__ == "__main__":
-    
+
     Puerto_RTP = []
     Metodos = ['INVITE', 'ACK', 'BYE']
     try:
@@ -74,7 +102,7 @@ if __name__ == "__main__":
     IP = uaserver.split(" ")[0][1:-1]
     #PUERTO
     uaserver1 = line_uaserver[0].split("=")[2]
-    PUERTO = uaserver1.split(" ")[0][1:-1]  
+    PUERTO = uaserver1.split(" ")[0][1:-1]
     #IP DEL PROXY
     line_regproxy = line[4].split(">")
     regproxy = line_regproxy[0].split("=")[1]
@@ -86,6 +114,10 @@ if __name__ == "__main__":
     line_rtp = line[3].split(">")
     rtpaudio = line_rtp[0].split("=")[1]
     PUERTO_AUDIO = rtpaudio.split(" ")[0][1:-1]
+    #PATH DEL LOG
+    line_log = line[5].split(">")
+    log = line_log[0].split("=")[1]
+    PATH_LOG = log.split(" ")[0][1:-1]
     #PATH DEL AUDIO
     line_audio = line[6].split(">")
     audio = line_audio[0].split("=")[1]
@@ -99,5 +131,10 @@ if __name__ == "__main__":
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((IP_PROXY, int(PUERTO_PROXY)))
 
-    serv = SocketServer.UDPServer(("127.0.0.1", int(PUERTO)), EchoHandler)
-    serv.serve_forever()
+    try:
+        serv = SocketServer.UDPServer(("127.0.0.1", int(PUERTO)), EchoHandler)
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        fich = open(PATH_LOG, 'a')
+        fich.write(str(time.time()) + " Finishing..." + '\r\n')
+        fich.close()
